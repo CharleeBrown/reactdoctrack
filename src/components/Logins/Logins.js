e22adf3d700b94d1471abc53fd8f32a36663ef55
Coleman <charleebrown22@gmail.com>
2022-07-13 13:03:38 -0500

    using props

--- src/components/Logins/Logins.js
+++ src/components/Logins/Logins.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Logins.module.css';
-import { Box, Center, Image, Flex, Badge, Text, Spacer} from '@chakra-ui/react';
+import { Input, Button, Box,Container, Center, Image, Flex, Badge, Text, Spacer, VStack} from '@chakra-ui/react';
 
-const Logins = () => (
+const Logins = (props) => (
+  
   <div className={styles.Logins}>
-    <Flex>
-        <Box bg='blue.500'>
-            Testing
-        </Box>
-    </Flex>
+    <VStack>
+      
+      <Container maxW='lg' bg='blue.600' color='white'>
+            <h1>Hello, {props.name}</h1>
+      </Container>
+        
+        <Spacer />
+        <Container maxW='lg' bg='blue.600' color='white' maxH='lg'>
+            <Box padding='4'>
+            <Input placeholder='Test' color='black'/>
+            <Button onClick={() => console.log("test")} colorScheme='blue'></Button>
+            </Box>
+        
+        </Container>
+    </VStack>
   </div>
 );
 
