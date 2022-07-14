42c0035ca1a25ecba6b3e30911f4c0cd0a6a1a5e
Coleman <charleebrown22@gmail.com>
2022-07-14 15:52:00 -0500

    using props

--- src/components/Logins copy/Logins.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Logins from './Logins';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Logins />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
