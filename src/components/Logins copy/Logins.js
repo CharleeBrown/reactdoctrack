81f796505d7bf1e96da8f046bb11e5316a302f2e
Coleman <charleebrown22@gmail.com>
2022-07-14 15:11:49 -0500

    updating packages

--- src/components/Logins copy/Logins.js
+++ src/components/Logins copy/Logins.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import styles from './Logins.module.css';
+import { Menu, 
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,Text, Spacer, VStack} from '@chakra-ui/react';
+import { getValue } from '@testing-library/user-event/dist/utils';
+import { restart } from 'nodemon';
 
 const Logins = () => (
   <div className={styles.Logins}>
-    <div className="container">
-      <input type="text" id="usrName"/>
-      
-    </div>
+    <Menu>
+    <MenuButton rightIcon={<ChevronDownIcon />}>
+      Options
+      </MenuButton>
+    </Menu>
   </div>
 );
 
