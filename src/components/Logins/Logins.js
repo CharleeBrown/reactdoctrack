d569eb4fea21b802bbae86b4bdaff219ea6d965a
Coleman <charleebrown22@gmail.com>
2022-07-14 13:53:45 -0500

    Attempting email validation

--- src/components/Logins/Logins.js
+++ src/components/Logins/Logins.js
@@ -6,22 +6,31 @@ import {   FormControl,
   FormErrorMessage,
   FormHelperText,  Input, Button, Box,Container, Center, Image, Flex, Badge, Text, Spacer, VStack} from '@chakra-ui/react';
 import { getValue } from '@testing-library/user-event/dist/utils';
+import { restart } from 'nodemon';
 
 function Logins(props) {
   let [count, setCount] = useState(0);
- 
+  const [emailError, setEmailError]=useState('');
+  let validEmail =new RegExp('/\S+@\S+\. \S+/');
+  if(validEmail.test(emailError)){
+    setEmailError(true);
+  }
   return(
   <div className={styles.Logins}>
             <FormControl>
+            
+              <Center>
               <Container maxW='lg'>
-              <FormLabel htmlFor='namer'> Hello, {props.name}   </FormLabel>
-                <Input id='namer'placeholder='Test' onChange={e => setCount(e.target.value)} color='black'/>
+                
+              <FormLabel  htmlFor='namer'> Enter Username {props.name}   </FormLabel>
+                <Input id='namer'placeholder='Test' type="email" onChange={e => setCount(e.target.value)} color='black'/>
+                <Spacer p={1.5}/>
               <FormLabel htmlFor='passwrd'>Enter Password</FormLabel>
                 <Input type='password' onChange={e => setCount(e.target.value)}/>
-                <FormHelperText>We'll never share your email.</FormHelperText>
-                <Button onClick={() => alert(count)} colorScheme='blue'>Submit Email</Button>
+                <Spacer p={1.5}/>
+                <Button onClick={() => alert(count)} colorScheme='blue'>Login</Button>
                 </Container>
-        <Spacer />
+          </Center>
         </FormControl>
   </div>
 );
